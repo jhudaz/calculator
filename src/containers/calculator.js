112f8dd5b62cfdb9278fd4a64f8e47400d50a0dd
Jaime Velez <jandres.vr@hotmail.com>
2019-01-24 13:57:53 -0500

    testing calculator component with cypress

--- src/containers/calculator.js
+++ src/containers/calculator.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { result} from '../actions/';
+import { result } from '../actions/';
 
 class Calculator extends Component {
     constructor(props) {
@@ -23,23 +23,23 @@ class Calculator extends Component {
 
     }
     sumar() {
-        this.props.result(`Suma: ${this.state.s1} + ${this.state.s2} = ${this.state.s1+this.state.s2}`);
+        this.props.result(`Suma: ${this.state.s1} + ${this.state.s2} = ${this.state.s1 + this.state.s2}`);
         this.setState({ s1: 0, s2: 0 });
     }
     multiplicar() {
-        this.props.result(`Multiplicacion: ${this.state.m1} * ${this.state.m2} = ${this.state.m1*this.state.m2}`);
+        this.props.result(`Multiplicacion: ${this.state.m1} * ${this.state.m2} = ${this.state.m1 * this.state.m2}`);
         this.setState({ m1: 0, m2: 0 });
     }
     dividir() {
-        if(this.state.d2===0){
+        if (this.state.d2 === 0) {
             this.props.result(`No se puede dividir entre 0!`);
-        }else{
-            this.props.result(`Division: ${this.state.d1} / ${this.state.d2} = ${this.state.d1/this.state.d2}`)
+        } else {
+            this.props.result(`Division: ${this.state.d1} / ${this.state.d2} = ${this.state.d1 / this.state.d2}`)
         }
         this.setState({ d1: 0, d2: 0 });
     }
     restar() {
-        this.props.result(`Resta: ${this.state.r1} - ${this.state.r2} = ${this.state.r1-this.state.r2}`);
+        this.props.result(`Resta: ${this.state.r1} - ${this.state.r2} = ${this.state.r1 - this.state.r2}`);
         this.setState({ r1: 0, r2: 0 });
     }
     render() {
@@ -48,34 +48,34 @@ class Calculator extends Component {
                 <div>
                     <label>Sumar</label>
                     <ul>
-                        <li><input type="number" value={this.state.s1} onChange={a => this.setState({ s1: +a.target.value })} /></li>
-                        <li><input type="number" value={this.state.s2} onChange={a => this.setState({ s2: +a.target.value })} /></li>
+                        <li><input className="suma1" type="number" value={this.state.s1} onChange={a => this.setState({ s1: +a.target.value })} /></li>
+                        <li><input className="suma2" type="number" value={this.state.s2} onChange={a => this.setState({ s2: +a.target.value })} /></li>
                     </ul>
-                    <button onClick={this.sumar}>Sumar</button>
+                    <button className="sumar" onClick={this.sumar}>Sumar</button>
                 </div>
                 <div>
                     <label>MUltiplicar</label>
                     <ul>
-                        <li><input type="number" value={this.state.m1} onChange={a => this.setState({ m1: +a.target.value })} /></li>
-                        <li><input type="number" value={this.state.m2} onChange={a => this.setState({ m2: +a.target.value })} /></li>
+                        <li><input className="multi1" type="number" value={this.state.m1} onChange={a => this.setState({ m1: +a.target.value })} /></li>
+                        <li><input className="multi2" type="number" value={this.state.m2} onChange={a => this.setState({ m2: +a.target.value })} /></li>
                     </ul>
-                    <button onClick={this.multiplicar}>Multiplicar</button>
+                    <button className="multiplicar" onClick={this.multiplicar}>Multiplicar</button>
                 </div>
                 <div>
                     <label>Restar</label>
                     <ul>
-                        <li><input type="number" value={this.state.r1} onChange={a => this.setState({ r1: +a.target.value })} /></li>
-                        <li><input type="number" value={this.state.r2} onChange={a => this.setState({ r2: +a.target.value })} /></li>
+                        <li><input className="resta1" type="number" value={this.state.r1} onChange={a => this.setState({ r1: +a.target.value })} /></li>
+                        <li><input className="resta2" type="number" value={this.state.r2} onChange={a => this.setState({ r2: +a.target.value })} /></li>
                     </ul>
-                    <button onClick={this.restar}>Restar</button>
+                    <button className="restar" onClick={this.restar}>Restar</button>
                 </div>
                 <div>
                     <label>Dividir</label>
                     <ul>
-                        <li><input type="number" value={this.state.d1} onChange={a => this.setState({ d1: +a.target.value })} /></li>
-                        <li><input type="number" value={this.state.d2} onChange={a => this.setState({ d2: +a.target.value })} /></li>
+                        <li><input className="divi1" type="number" value={this.state.d1} onChange={a => this.setState({ d1: +a.target.value })} /></li>
+                        <li><input className="divi2" type="number" value={this.state.d2} onChange={a => this.setState({ d2: +a.target.value })} /></li>
                     </ul>
-                    <button onClick={this.dividir}>Dividir</button>
+                    <button className="dividir" onClick={this.dividir}>Dividir</button>
                 </div>
 
             </div>
