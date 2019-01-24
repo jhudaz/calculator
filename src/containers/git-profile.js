c3daaa2717e66048cccb0dc737222e903e25118d
Jaime Velez <jandres.vr@hotmail.com>
2019-01-24 14:18:34 -0500

    setting autofocus on github profile input text and the using the event keyUp on the botton

--- src/containers/git-profile.js
+++ src/containers/git-profile.js
@@ -9,24 +9,30 @@ class GitProfile extends Component {
         this.state = {
             userName: ''
         };
+    this.handleKeyUp = this.handleKeyUp.bind(this);
     }
     createList(data, i) {
         return (
             <li>{data}</li>
         );
     }
+    handleKeyUp(event) {
+        if (event.keyCode === 13) {
+            this.props.gitProfile(this.state.userName)
+        }
+    }
     render() {
         return (
             <div>
                 <label>Git User:</label>
                 <br />
-                <input type="text" placeholder='user' value={this.state.userName} onChange={e => this.setState({ userName: e.target.value })} />
+                <input autofocus="true" type="text" placeholder='user' onKeyUp={this.handleKeyUp} value={this.state.userName} onChange={e => this.setState({ userName: e.target.value })} />
                 <button onClick={() => this.props.gitProfile(this.state.userName)}>Find</button>
                 <hr />
                 <h2>Profile</h2>
                 <ul>
                     <li>name:{this.props.gitProfileReducer.name}</li>
-                    <li><img src={this.props.gitProfileReducer.avatar_url} alt="profile"/></li>
+                    <li><img src={this.props.gitProfileReducer.avatar_url} alt="profile" /></li>
                     <li>location: {this.props.gitProfileReducer.location}</li>
                     <li>description: {this.props.gitProfileReducer.bio}</li>
                     <li>id: {this.props.gitProfileReducer.id}</li>
