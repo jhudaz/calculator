06e5d51aa7ac237c913b61f57a0e34d95ec4e079
jvelezpo <sebasvelez@gmail.com>
2019-01-31 14:01:59 -0500

    props

--- front/src/reducers/reducer-consume-api.js
+++ front/src/reducers/reducer-consume-api.js
@@ -1,22 +1,25 @@
 function getInitialStatus() {
-    return {
-        userList: [],
-        user: { firstName:'',lastName:''}
-    };
-}
-export default function (status = getInitialStatus(), action) {
-    switch (action.type) {
-        case 'CONSUME_API_GET':
-            return {
-                ...status,
-                userList: action.payload
-            };
-        case 'CONSUME_API_GET_ONE':
-            return {
-                ...status,
-                user: action.payload
-            }
-        default:
-            return status;
+  return {
+    userList: [],
+    user: {
+      firstName: '',
+      lastName: ''
     }
+  };
+}
+export default function(status = getInitialStatus(), action) {
+  switch (action.type) {
+    case "CONSUME_API_GET":
+      return {
+        ...status,
+        userList: action.payload
+      };
+    case "CONSUME_API_GET_ONE":
+      return {
+        ...status,
+        user: action.payload
+      };
+    default:
+      return status;
+  }
 }
