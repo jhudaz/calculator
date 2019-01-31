5b86545e205cef5ee4430a23383c6998f3da957c
Jaime Velez <jandres.vr@hotmail.com>
2019-01-31 16:59:36 -0500

    more changes

--- back/models/index.js
+++ back/models/index.js
@@ -9,9 +9,6 @@ const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 
 let sequelize;
-console.log('------------');
-console.log('config', config);
-console.log('------------');
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
